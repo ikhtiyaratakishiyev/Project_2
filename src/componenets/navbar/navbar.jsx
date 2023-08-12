0b7df83bf5a005b6d8700c117a7dc98732112fe7
ikhtiyaratakishiyev <ikhtiyar.atakishiyev@gmail.com>
2023-08-13 01:05:10 +0400

    Finished navbar

--- src/componenets/navbar/navbar.jsx
+++ src/componenets/navbar/navbar.jsx
@@ -23,32 +23,46 @@ const Navbar = () => {
             <img src={logo} alt="logo" />
           </div>
           <div className="project_2_navbar-links_container">
-           <Menu/>
+            <p><a href="#home">Home</a></p>
+            <p><a href="#whatProject_2">What is GPT?</a></p>
+            <p><a href="#possibility">Open AI</a></p>
+            <p><a href="#features">Case Studies</a></p>
+             <p><a href="#blog">Library</a></p>
           </div>
-          <div className="project_2__navbar-sign">
+        </div>
+
+        <div className="project_2__navbar-sign">
             <p>Sign in</p>
             <button type='button'>Sign up</button>
           </div>
 
-          <div className="project_3__navbar-menu">
-            {toggleMenu
-            ?<RiCloseLine color='#fff' size={27} onClick={()=>setToggleMenu(false)}/>
-            :<RiMenu3Line color='#fff' size={27} onClick={()=>setToggleMenu(true)}/>
-            }
-            {/* {toggleMenu &&(
-              <div className='project_2__navbar-menu-container scale-up-container'>
+          <div className="project_2__navbar-menu">
+          {toggleMenu
+          ? <RiCloseLine color="#fff" size={27} onClick={() => setToggleMenu(false)} />
+          : <RiMenu3Line color="#fff" size={27} onClick={() => setToggleMenu(true)} />}
+
+
+            {toggleMenu &&(
+              <div className='project_2__navbar-menu_container scale-up-center'>
                 <div className='project_2__navbar-menu_container-links'>
-                   <Menu/>
-                   <div className="project_2__navbar-menu_container-links-sign">
-            <p>Sign in</p>
-            <button type='button'>Sign up</button>
-          </div>
+                <p><a href="#home">Home</a></p>
+                <p><a href="#whatProject_2">What is GPT?</a></p>
+                <p><a href="#possibility">Open AI</a></p>
+                <p><a href="#features">Case Studies</a></p>
+                <p><a href="#blog">Library</a></p>
+                <div className="project_2__navbar-menu_container-links-sign">
+                       <p>Sign in</p>
+                      <button type='button'>Sign up</button>
+                    </div>
                 </div>
+                     
               </div>
-            )} */}
+            )}
+
+       
           </div>
         </div>        
-    </div>
+    
   )
 }
 
